0a125bc280c83dd3ba11b0c93fc76764c4ed77c6
Luis Pena <lpenapec@gmail.com>
2025-02-20 23:18:30 -0600

    link for card detail page

--- src/components/MainCardSingle.vue
+++ src/components/MainCardSingle.vue
@@ -10,6 +10,7 @@ const username = faker.internet.userName({ firstName: firstname, lastName: lastn
 </script>
 
 <template>
+    <RouterLink :to="{ name: 'CardDetail', params: {id: username}}">
     <div class="border border-gray-500 rounded-lg shawdow bg-gray-400 h-full">
         <img class="object-fill h-480 w-960 rounded-t-lg" v-bind:src="faker.image.urlLoremFlickr({category: 'cats', height: 480, width: 960 })"/>
         <div class="p-4">
@@ -18,4 +19,5 @@ const username = faker.internet.userName({ firstName: firstname, lastName: lastn
             <p class="mb-3 font-normal text-gray-800">{{ bio }}</p>
         </div>
     </div>
+    </RouterLink>
 </template>
